c5f22fa2981966cd1327c9648205604c89d459b5
max.lee <max.lee@ruckuswireless.com>
2020-10-04 21:48:20 +0800

    add name Hill

--- src/com/dockerbyexample/HelloThere.java
+++ src/com/dockerbyexample/HelloThere.java
@@ -3,7 +3,7 @@ package com.dockerbyexample;
 import java.util.Random;
 
 public class HelloThere {
-    final static String NAME[] = {"Laurus", "Tilia", "Elliottia"};
+    final static String NAME[] = {"Laurus", "Tilia", "Elliottia", "Hill"};
     public static void main(String[] args) {
         Random rand = new Random();
         System.out.println("Hello there! " + NAME[rand.nextInt(NAME.length)]);
