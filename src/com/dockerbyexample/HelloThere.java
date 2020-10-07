9bc1347f8846a527d9f4475b1d21d56f409d09ab
Max Lee <max.lee@ruckuswireless.com>
2020-10-07 12:29:56 +0800

    add Douglas

--- src/com/dockerbyexample/HelloThere.java
+++ src/com/dockerbyexample/HelloThere.java
@@ -3,7 +3,7 @@ package com.dockerbyexample;
 import java.util.Random;
 
 public class HelloThere {
-    final static String NAME[] = {"Laurus", "Tilia", "Elliottia", "Hill"};
+    final static String NAME[] = {"Laurus", "Tilia", "Elliottia", "Hill", "Douglas"};
     public static void main(String[] args) {
         Random rand = new Random();
         System.out.println("Hello there! " + NAME[rand.nextInt(NAME.length)]);
